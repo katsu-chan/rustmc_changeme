e13cd23eef107620091dc11ecc00ee3d0b012a3a
Eveeee <44003480+katsu-chan@users.noreply.github.com>
2023-08-29 00:34:07 +0300

    Yay i can send messages

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-use ozelot::Client;
+use ozelot::{Client, serverbound::{self}};
 
 fn main() {
-    let mut client = Client::connect_unauthenticated("localhost", 25565, "dev").unwrap();
-    loop {
-        for packet in client.read().unwrap() {
-            print!("{:?}", packet);
-        }
-    }
+    let mut client = Client::connect_unauthenticated("localhost", 25566, "dev").unwrap();
+    //loop {
+    //    for packet in client.read().unwrap() {
+    //        print!("{:?}", packet);
+    //    }
+    //}
+
+    let _ = client.send(serverbound::ChatMessage::new("Hiiiiiii!!".to_owned()));
+    let _ = client.write();
     client.close().unwrap();
     println!("Hello, world!");
 }
